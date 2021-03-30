928e8443753763da9dd4b65d69c808d3ffffb983
elboraki <younes.elboraki@gmail.com>
2021-03-31 00:19:28 +0100

    crud api blog

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -3,38 +3,38 @@ import { Body, Controller, Delete, Get, Logger, Param, Post, Put } from '@nestjs
 @Controller('blog')
 export class BlogController {
     @Get()
-    getAll(){
-     
-        Logger.log("get ALL articles","BlogController");
+    getAll() {
+
+        Logger.log("get ALL articless", "BlogController");
 
         return "Get all article";
     }
 
     @Get(':articleId')
-    getOne(@Param('articleId') articleId){
-     
-        Logger.log("get one article","BlogController");
+    getOne(@Param('articleId') articleId) {
+
+        Logger.log("get one article", "BlogController");
 
         return "Get one article";
     }
 
     @Post()
-    create(@Body() articleDto){
-        Logger.log("create an article","BlogController");
+    create(@Body() articleDto) {
+        Logger.log("create an article", "BlogController");
 
         return "create article";
     }
 
     @Put(':articleId')
-    update(@Param(":articleId") articleId,@Body() articleDto){
-        Logger.log("update an article","BlogController");
+    update(@Param(":articleId") articleId, @Body() articleDto) {
+        Logger.log("update an article", "BlogController");
 
         return "update article";
     }
 
     @Delete(":articleId")
-    delete(@Param("articleId") articleId){
-        Logger.log("delete an article","BlogController");
+    delete(@Param("articleId") articleId) {
+        Logger.log("delete an article", "BlogController");
 
         return "delete article";
     }
